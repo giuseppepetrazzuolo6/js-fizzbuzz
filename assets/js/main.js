4d599b494d93c994dd5d93ad791d8f57ec740d43
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-07 14:06:04 +0200

    add comment for tools to use and task to do

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 /* Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+/*TOOLS
+- for
+- if; else
+- operatori di base (<;>;=...)
+- operatore modulo %
+
+TASK
+- ciclo for composto da iniziallizazione, condizione e incremento/decremento 
+- prima condizione mutipli di 3 e 5
+- seconda condizione solo multipli di 3
+- terza condizione solo multipli di 5
+- quarta condizione numeri non multipli di 3 e/o 5
+*/
